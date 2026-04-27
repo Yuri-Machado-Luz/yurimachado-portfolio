d48756c50be4f64aaabbe44c8ed7f85213550a63
Yuri Machado Luz <yurimachadoluz@gmail.com>
2026-04-27 20:31:47 -0300

    first commit - iniciado com template StarLight

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,25 @@
-import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { docsLoader } from "@astrojs/starlight/loaders";
+import { docsSchema } from "@astrojs/starlight/schema";
+import { defineCollection } from "astro:content";
+
+// // ===
+
+// function projectsCollection() {
+//   return z.object({
+//     title: z.string(),
+//     description: z.string(),
+//     lastModified: z.date(),
+//     status: z.enum(["Ativo", "Arquivado", "Em desenvolvimento"]),
+//     repository: z.string().optional(),
+//     category: z.enum(["Pessoal", "Trabalho", "Open Source"]).optional(),
+//   });
+// }
+
+// // ===
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema({}),
+  }),
 };
